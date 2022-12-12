085cfd6a99078d1d5dfdab7a49779ffe4bc911a3
anddp12 <anddp12@gmail.com>
2022-12-12 16:38:59 +0200

    add calculation area

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ class Figure:
     def __init__(self, name) -> None:
         self.name = name
 
+    def area(self):
+        return
+
     def print(self):
         print(f'Figure: {self.name}')
 
@@ -10,22 +13,34 @@ class Square(Figure):
         super().__init__('Square')
         self.a = a
 
+    def area(self):
+        return self.a **2
+
 class Rectangle(Figure):
     def __init__(self, a, b) -> None:
         super().__init__('Rectangle')
         self.a = a
         self.b = b
 
+    def area(self):
+        return self.a * self.b
+
 class Circle(Figure):
     def __init__(self, r) -> None:
         super().__init__('Cicle')
         self.r = r
+    
+    def area(self):
+        return 3.14 * (self.r **2)
 
 class Ellipse(Figure):
     def __init__(self, R, r) -> None:
         super().__init__('Ellipse')
         self.R = R
         self.r = r
+
+    def area(self):
+        return 3.14 * self.R * self.r
 
 sq = Square(10)
 sq.print()
